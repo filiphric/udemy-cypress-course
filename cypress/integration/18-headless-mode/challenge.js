926047f2cd25edd4d470f5ca6a8a7f29408ef742
Filip Hric <filip@slido.com>
2020-04-18 14:21:08 +0200

    add headless run challenge

---
+++ cypress/integration/18-headless-mode/challenge.js
@@ -0,0 +1,12 @@
+/* 
+  🚀 challenge #1: open cypress.json file and try to change 
+  "testFiles" option so that you will not need to pass the
+  --spec argument when running npx cypress run command
+*/ 
+
+/* 
+  🚀 challenge #2: go to http://on.cypress.io/cli#cypress-run and
+  look into different options you can pass on npx cypress run 
+  command. try chaning browser or env variables by passing 
+  arguments to the npx cypress run command
+*/ 
